96b6d55fa041d0941dc278b2261acaedf0658453
Shivani Arunachalam <shivaniarunachalam10@gmail.com>
2022-05-26 13:42:39 +0530

    Initial Commit

---
+++ CommandsAPI/Controllers/CommandsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommandsAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommandsController : Controller
+    {
+       
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "this", "is", "a", "sample" };
+        }
+    }
+}
